aac59c482b7db5c840d253607eb683f8076b316a
yseij <r0790156@student.thomasmore.be>
2022-05-05 18:42:45 +0200

    Add project files.

---
+++ KraanDevExpress.Module/BusinessObjects/Klant.cs
@@ -0,0 +1,69 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace KraanDevExpress.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class Klant : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public Klant(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+        private string _name;
+        [RuleRequiredField]
+        public string Name
+        {
+            get { return _name; }
+            set { SetPropertyValue(nameof(Name), ref _name, value); }
+        }
+
+        private string _basisUrl1;
+        [RuleRequiredField]
+        public string BasisUrl1
+        {
+            get { return _basisUrl1; }
+            set { SetPropertyValue(nameof(BasisUrl1), ref _basisUrl1, value); }
+        }
+
+        private string _basisUrl2;
+        public string BasisUrl2
+        {
+            get { return _basisUrl2; }
+            set { SetPropertyValue(nameof(BasisUrl2), ref _basisUrl2, value); }
+        }
+
+        [Association]
+        public XPCollection<Webservice> Webservice
+        {
+            get
+            {
+                return GetCollection<Webservice>(nameof(Webservice));
+            }
+        }
+    }
+}
---
+++ KraanDevExpress.Module/BusinessObjects/Webservice.cs
@@ -0,0 +1,68 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace KraanDevExpress.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class Webservice : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public Webservice(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+        private string _name;
+        [RuleRequiredField]
+        public string Name
+        {
+            get { return _name; }
+            set { SetPropertyValue(nameof(Name), ref _name, value); }
+        }
+
+        private bool _soap;
+        [RuleRequiredField]
+        public bool Soap
+        {
+            get { return _soap; }
+            set { SetPropertyValue(nameof(Soap), ref _soap, value); }
+        }
+
+        private string _securityId;
+        public string SecurityId
+        {
+            get { return _securityId; }
+            set { SetPropertyValue(nameof(_securityId), ref _securityId, value); }
+        }
+
+        [Association]
+        public XPCollection<Klant> klant
+        {
+            get
+            {
+                return GetCollection<Klant>(nameof(klant));
+            }
+        }
+    }
+}
